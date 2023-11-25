536335cee8002099a638f45cbeade77360747313
guilhermesanttiago <guilherme.santiago@spetch.school>
2023-11-25 17:43:45 -0300

    Adicionei as perguntas

---
+++ site/public/js/question.js
@@ -0,0 +1,32 @@
+var quiz = [
+
+    {
+      q: "Qual é o clã de Sasuke?",
+      opcoes: ['Uzumaki', 'Hyuga', 'Uchiha', 'Nara'],
+      resposta: 2
+    },
+  
+    {
+      q: "Qual é o nome do sensei de Naruto na Academia Ninja?",
+      opcoes: ['Jiraiya', 'Iruka', 'Kakashi', 'Tsunade'],
+      resposta: 1
+    },   
+  
+    {
+      q: "Qual é o elemento de chakra principal de Naruto?",
+      opcoes: ['Fogo', 'Água', 'Vento', 'Trovão'],
+      resposta: 2
+    },
+  
+    {
+      q: 'Quem é conhecido por ser o "Ninja Copiador"?',
+      opcoes: ['Kakashi Hatake', 'Sasuke Uchiha', 'Itachi Uchiha', 'Neji Hyuga'],
+      resposta: 0
+    },
+  
+    {
+      q: 'O que significa o nome "Hokage"?"',
+      opcoes: ['Sombra', 'Fogo', 'Trovão', 'Fogo de Deus'],
+      resposta: 3
+    }
+]
