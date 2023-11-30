80e45ebd9ef5f112bc3c1e27fa647936b3c58387
guilhermesanttiago <guilherme.santiago@spetch.school>
2023-11-30 01:02:21 -0300

    Alterações gerais

--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+
 var usuarioController = require("../controllers/usuarioController");
+
 
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
@@ -17,6 +19,9 @@ router.post("/autenticar", function (req, res) {
 router.get("/estatistica", function (req, res) {
     usuarioController.estatistica(req, res);
 });
+
+
+
 
 
 module.exports = router;
